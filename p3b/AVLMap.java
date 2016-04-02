2be689dc63bf4e351666f78f8c390af9c6dd677a
Karl Tayeb <karl.tayeb7@gmail.com>
2016-04-02 00:13:16 -0400

    double rotations aren't working

--- p3b/AVLMap.java
+++ p3b/AVLMap.java
@@ -14,17 +14,105 @@ public class AVLMap<K extends Comparable<? super K>, V> extends BSTMap<K, V>{
      *  @param val the value of the entry
      *  @return the original value associated with the key, or null if not found
      */
-    @Override
+    @Override()
     public V put(K key, V val) {
         this.modified = true;
-        V temp = this.put(key, val, this.root);
-        this.updateHeight(this.root);
-        while (!this.isBalanced()){ //runs at most twice
-        	//this.balanceroutine(this.root);
-        	break;
-        }
-        return temp;
+        return this.put(key, val, this.root);
     }
+
+    /** Put <key,value, curr> entry into subtree with given root node.
+     *  @throws IllegalArgumentException if you give it a null key/val
+     *  @param key the key of the entry
+     *  @param val the value of the entry
+     *  @param curr the root of the subtree into which to put the entry
+     *  @return the original value associated with the key, or null if not found
+     */
+    protected V put(K key, V val, BNode curr) throws IllegalArgumentException {
+        this.modified = true;
+        if (key == null || val == null) {
+            throw new IllegalArgumentException();
+        }
+        /* If the map has the key, replace the value */
+        if (this.hasKey(key, curr)) {
+            return this.find(key, curr).setValue(val);
+        }
+        
+        /* Otherwise make a new node and put it in the tree */
+        BNode node = new BNode(key, val);
+        node.right = this.leaf;
+        node.left = this.leaf;
+        this.insert(node, this.root);
+        return null;
+    }
+
+    @Override
+    protected void insert(BNode node, BNode curr) {
+        this.modified = true;
+        /* If we try to insert on a sentinal, replace the sentinal.
+         * since we inspect left and right children, this only happens when
+         * inserting at the root of the tree. */
+        if (curr.isLeaf()) {
+            this.root = node;
+            this.size++;
+            return;
+        }
+        /* Otherwise move right or left and insert*/
+        if (node.key.compareTo(curr.key) < 0) {
+            if (curr.left.isLeaf()) {
+                curr.left = node;
+                this.size++;
+                return;
+            }
+            this.insert(node, curr.left);
+        } else {
+            if (curr.right.isLeaf()) {
+                curr.right = node;
+                this.size++;
+                return;
+            }
+            this.insert(node, curr.right);
+        }
+        
+        this.updateHeight(curr);
+        int bf = this.balanceFactor(curr);
+        if (bf > 1) {
+        	if (this.balanceFactor(curr.left) == -1) {
+        		curr.left = leftrotate(curr.left);
+        	}
+        	curr = this.rightrotate(curr);
+        	return;
+        }
+        if (bf < -1) {
+        	if (this.balanceFactor(curr.right) == 1) {
+        		curr.left = rightrotate(curr.right);
+        	}
+        	curr = this.leftrotate(curr);
+        	return;
+        }
+
+        return;
+    }
+
+    /** Find and return the node with a key in a subtree, null if not found.
+     * @param key the key to look for
+     * @param curr the root of the subtree we're searching
+     * @return the node we find
+     */
+    protected BNode find(K key, BNode curr) {
+        if (curr.isLeaf()) {
+            return this.leaf;
+        }
+        if (key.compareTo(curr.key) < 0) {
+            return this.find(key, curr.left);
+        } else if (key.compareTo(curr.key) == 0) {
+            return curr;
+        } else {
+            return this.find(key, curr.right);
+        }
+    }
+
+
+
 
     /** Recursively updates the heights of nodes in the tree
      * @param curr the current node in consideration
@@ -65,13 +153,52 @@ public class AVLMap<K extends Comparable<? super K>, V> extends BSTMap<K, V>{
     	if (curr == null || curr.isLeaf()) {
     		return 0;
     	}
-    	return curr.right.height - curr.left.height;
+    	return curr.left.height - curr.right.height;
     }
 
-    private void balanceroutine() {
-
+    private void removebalanceroutine(BNode curr) {
+    	/**
+    	// Descend to the lowest unbalanced subtree
+    	while(!this.isBalanced(curr)) {
+    		this.balanceroutine(curr.left);
+    		this.balanceroutine(curr.right);
+    	}
+    	if (this.isBalanced(curr)) {
+    		return;
+    	}
+    	if (this.balanceFactor(curr) < -1) {
+    		curr = this.leftrotate(curr);
+    		return;
+    	} else {
+    		curr = this.rightrotat(curr);
+    		return;
+    	}
+    	*/
+    	return;
     }
 
+    private BNode rightrotate(BNode pivot) {
+    	BNode up = pivot.left;
+    	BNode temp = up.right;
+    	if (this.root == pivot) {
+    		this.root = up;
+    	}
+    	up.right = pivot;
+    	pivot.left = temp;
+    	this.updateHeight(up);
+    	return up;
+    }
+    private BNode leftrotate(BNode pivot) {
+    	BNode up = pivot.right;
+    	BNode temp = up.left;
+    	if (this.root == pivot) {
+    		this.root = up;
+    	}
+    	up.left = pivot;
+    	pivot.right = temp;
+    	this.updateHeight(up);
+    	return up;
+    }
 
     /** Returns a copy of the portion of this map whose keys are in a range.
      *  @param fromKey the starting key of the range, inclusive if found
--- p3b/test.java
+++ p3b/test.java
@@ -6,7 +6,7 @@ public class test {
 		System.out.println(map);
 		System.out.println(map.isBalanced());
 
-		AVLMap<Integer, String> submap = map.subMap(400,1024);
+		AVLMap<Integer, String> submap = map.subMap(400,1028);
 		System.out.println(submap);
 		System.out.println(submap.balanceFactor());
 		System.out.println(submap.isBalanced());
@@ -14,17 +14,27 @@ public class test {
 
 	private static AVLMap<Integer, String> makeMap() {
 		AVLMap<Integer, String> map = new AVLMap();
-		map.put(32, "32");
-		map.root();
-		map.put(256, "256");
-		map.put(512, "512");
+	
+		//map.put(512, "512");
+
+		//map.put(256, "256");
+		map.put(4, "y");
+		map.put(32, "y");
+		map.put(8, "y");	
+		//map.put(2, "2");
+		/*
 		map.put(1024, "1024");
 		map.put(128, "128");
 		map.put(64, "64");
 		map.put(8, "8");
 		map.put(16, "16");
 		map.put(2, "2");
-		map.put(4, "4");
+
+
+		map.put(400, "400");
+		map.put(1028, "1028");
+		*/
+		map.root();
 		return map;
 	}
 
